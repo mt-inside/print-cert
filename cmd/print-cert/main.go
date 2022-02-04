2f74aa0a7fad70fbaf5c7ae670780dc5e15447b3
Matt Turner <m.turner@mwam.com>
2022-02-04 10:25:30 +0000

    Initial commit

---
+++ cmd/print-cert/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"bitbucket.mwam.local/infra/lb-checker/pkg/utils"
+	. "bitbucket.mwam.local/infra/lb-checker/pkg/utils"
+)
+
+/* TODO
+* - when checking SAN validity, also check subj (need to parse that CN)
+* - show key len
+* - show dnssec and dane status of name
+* - use -h for sni, unless -s is given
+* - use "-a http" to do an http get /, print server, content-type, body-len (for this, drop the manual NextProtos, give the tls config to an http transport, give that to http client, and use that to do the get. Somewhere (the response object?) you can get hold of the conn object
+* - see if the address parses as an IP. IP or name, print the reverse (and go again and again until you see one you've seen before, print them all)
+ */
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: addr port")
+		os.Exit(1)
+	}
+
+	addr := os.Args[1]
+	port := os.Args[2]
+
+	var ip net.IP
+	var host string
+
+	ip = net.ParseIP(addr)
+	if ip == nil {
+		host = addr
+		ip = CheckDns(host)
+	} else {
+		host = CheckRevDns(ip)
+	}
+
+	/* Check DNS */
+
+	Banner("DNS")
+
+	// f5RevHost := checkRevDns(f5Ip)
+	// checkDnsConsistent(f5Host, f5RevHost)
+
+	// nsHost := checkRevDns(nsIp)
+	// nsRevIp := checkDns(nsHost)
+	// checkDnsConsistent(nsIp.String(), nsRevIp.String())
+
+	/* Check F5 */
+
+	tcpAddr := net.JoinHostPort(addr, port)
+	utils.CheckTls2(tcpAddr, addr) // TODO support -h host, for when giving an ip
+
+	/* Fin */
+
+	fmt.Println()
+	fmt.Println()
+
+	os.Exit(0)
+}
---
+++ pkg/utils/tests.go
@@ -0,0 +1,231 @@
+package utils
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func CheckDns(name string) net.IP {
+	ips, err := net.LookupIP(name)
+	if ok := CheckWarn(err); !ok {
+		return net.IPv4(0, 0, 0, 0)
+	}
+	if len(ips) > 1 {
+		CheckErr(errors.New("host resolves to >1 IP"))
+	}
+	ip := ips[0]
+	fmt.Printf("Name %v is %s\n", AddrStyle.Render(name), AddrStyle.Render(ip.String()))
+
+	return ip
+}
+
+func CheckRevDns(ip net.IP) string {
+	names, err := net.LookupAddr(ip.String())
+	if ok := CheckWarn(err); !ok {
+		return "<NXDOMAIN>"
+	}
+	if len(names) > 1 {
+		CheckErr(errors.New("IP resolves to >1 host"))
+	}
+	revName := names[0]
+	fmt.Printf("%s reverses to %s\n", AddrStyle.Render(ip.String()), AddrStyle.Render(revName))
+
+	return revName
+}
+
+// TODO does single-ip need this, or can it just use CheckTls2?
+func CheckTls(l4Addr string, host string) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         host, // SNI for TLS vhosting
+		},
+		TLSHandshakeTimeout:   1 * time.Second, // assume this includes TCP handshake
+		ResponseHeaderTimeout: 5 * time.Second,
+	}
+	client := &http.Client{Transport: tr}
+
+	fmt.Printf("%s TLS handshake with %s (SNI ServerName %s)...\n", STrying, AddrStyle.Render(l4Addr), AddrStyle.Render(host))
+	// TODO use context
+	resp, err := client.Get("https://" + l4Addr) // TODO construct a URL object
+	CheckErr(err)
+	defer resp.Body.Close()
+
+	cs := resp.TLS
+	fmt.Printf("%s TLS handshake with %s. TLS version %s; ALPN proto %s\n", SOk, AddrStyle.Render(l4Addr), versionName(cs.Version), cs.NegotiatedProtocol)
+	fmt.Printf("\t%s TLS cypher suite %s\n", SInfo, tls.CipherSuiteName(cs.CipherSuite))
+
+	/* Cert chain */
+
+	servingCert := cs.PeerCertificates[0]
+	fmt.Printf("Serving Cert [%s -> %s] %s subj %s (iss %s %s) ca %t\n",
+		TimeStyle.Render(servingCert.NotBefore.Format(TimeFmt)), TimeStyle.Render(servingCert.NotAfter.Format(TimeFmt)),
+		servingCert.PublicKeyAlgorithm, AddrStyle.Render(servingCert.Subject.String()),
+		AddrStyle.Render(renderIssuer(servingCert)), servingCert.SignatureAlgorithm,
+		servingCert.IsCA,
+	)
+	fmt.Printf("\tSANs: DNS %s, IPs %s\n",
+		AddrStyle.Render(strings.Join(servingCert.DNSNames, ",")), AddrStyle.Render(strings.Join(ips2str(servingCert.IPAddresses), ",")),
+	)
+	if !nameInSans(host, servingCert.DNSNames) { // TODO if it's an IP, check against IP SANs instead
+		fmt.Printf("\t%s given name %s not in SANs\n", SWarning, host)
+	}
+
+	fmt.Printf("Cert chain\n")
+	// TODO render first and subsequent certs differently (don't care about SANs on signers)
+	for _, cert := range cs.PeerCertificates[1:] {
+		fmt.Printf("\tCert [%s -> %s] %s subj %s (iss %s %s) ca %t\n",
+			TimeStyle.Render(cert.NotBefore.Format(TimeFmt)), TimeStyle.Render(cert.NotAfter.Format(TimeFmt)),
+			cert.PublicKeyAlgorithm, AddrStyle.Render(cert.Subject.String()),
+			AddrStyle.Render(renderIssuer(cert)), cert.SignatureAlgorithm,
+			cert.IsCA,
+		)
+	}
+}
+
+func CheckTls2(l4Addr string, host string) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+				ServerName:         host, // SNI for TLS vhosting
+			},
+			TLSHandshakeTimeout:   1 * time.Second, // assume this includes TCP handshake
+			ResponseHeaderTimeout: 5 * time.Second,
+			DisableCompression:    true,
+		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Printf("\t%s Redirected to %s\n", SInfo, AddrStyle.Render(req.URL.String()))
+			return nil
+		},
+	}
+
+	fmt.Printf("%s TLS handshake with %s (SNI ServerName %s, HTTP Host %s)...\n", STrying, AddrStyle.Render(l4Addr), AddrStyle.Render(host), AddrStyle.Render(l4Addr))
+	// TODO set host header
+	// TODO: not negoiating h2, looks like its sending no ALPN (and if we manually do it barfs on a binary response with i think is h2 when it's only expecting h1.1). Use go/x/net/http2 to do http2
+	l7Addr := url.URL{
+		Scheme: "https",
+		Host:   l4Addr,
+		Path:   "/",
+	}
+	req, err := http.NewRequest("GET", l7Addr.String(), nil)
+	CheckErr(err)
+	req.Host = l4Addr
+	resp, err := client.Do(req)
+	CheckErr(err)
+	defer resp.Body.Close()
+
+	cs := resp.TLS
+	fmt.Printf("%s TLS handshake with %s. %s; ALPN proto %s\n", SOk, AddrStyle.Render(l4Addr), versionName(cs.Version), cs.NegotiatedProtocol)
+	fmt.Printf("\t%s TLS cypher suite %s\n", SInfo, tls.CipherSuiteName(cs.CipherSuite))
+
+	/* Cert chain */
+
+	servingCert := cs.PeerCertificates[0]
+	fmt.Printf("Serving Cert [%s -> %s] %s subj %s (iss %s %s) ca %t\n",
+		TimeStyle.Render(servingCert.NotBefore.Format(TimeFmt)), TimeStyle.Render(servingCert.NotAfter.Format(TimeFmt)),
+		servingCert.PublicKeyAlgorithm, AddrStyle.Render(servingCert.Subject.String()),
+		AddrStyle.Render(renderIssuer(servingCert)), servingCert.SignatureAlgorithm,
+		servingCert.IsCA,
+	)
+	fmt.Printf("\tDNS SANs %s\n", AddrStyle.Render(strings.Join(servingCert.DNSNames, ",")))
+	fmt.Printf("\tIP SANs %s\n", AddrStyle.Render(strings.Join(ips2str(servingCert.IPAddresses), ",")))
+	if !nameInSans(host, servingCert.DNSNames) { // TODO if it's an IP, check against IP SANs instead
+		fmt.Printf("\t%s given name %s not in SANs\n", SWarning, host)
+	}
+
+	if len(cs.OCSPResponse) > 0 {
+		fmt.Printf("\t%s OSCP info stapled to response\n", SInfo)
+	}
+
+	fmt.Printf("Cert chain\n")
+	// TODO render first and subsequent certs differently (don't care about SANs on signers)
+	for _, cert := range cs.PeerCertificates[1:] {
+		fmt.Printf("\tCert [%s -> %s] %s subj %s (iss %s %s) ca %t\n",
+			TimeStyle.Render(cert.NotBefore.Format(TimeFmt)), TimeStyle.Render(cert.NotAfter.Format(TimeFmt)),
+			cert.PublicKeyAlgorithm, AddrStyle.Render(cert.Subject.String()),
+			AddrStyle.Render(renderIssuer(cert)), cert.SignatureAlgorithm,
+			cert.IsCA,
+		)
+	}
+	if len(cs.VerifiedChains) > 0 {
+		// TODO: add cs.VerifidChains, which adds the certs from the local store that the presented certs (above) were verified against
+		panic("first time we've seen this, check it works")
+		if len(cs.VerifiedChains) > 1 {
+			panic("multiple chains")
+		}
+		for _, cert := range cs.VerifiedChains[0] {
+			fmt.Printf("\tCert [%s -> %s] %s subj %s (iss %s %s) ca %t\n",
+				TimeStyle.Render(cert.NotBefore.Format(TimeFmt)), TimeStyle.Render(cert.NotAfter.Format(TimeFmt)),
+				cert.PublicKeyAlgorithm, AddrStyle.Render(cert.Subject.String()),
+				AddrStyle.Render(renderIssuer(cert)), cert.SignatureAlgorithm,
+				cert.IsCA,
+			)
+		}
+	}
+
+	fmt.Println()
+
+	fmt.Printf("<= %s %s\n", resp.Proto, resp.Status)
+
+	// TODO: HSTS (is a header?)
+
+	// TODO: cors headers
+
+	fmt.Printf("\t%s %d bytes of %s from %s\n", SInfo, resp.ContentLength, resp.Header.Get("content-type"), resp.Header.Get("server"))
+	if resp.Uncompressed {
+		fmt.Printf("\t%s Note: content was transparently decompressed; length information will not be accurate\n")
+	}
+
+	rawBody, err := ioutil.ReadAll(resp.Body)
+	CheckErr(err)
+	fmt.Printf("\t%s %d bytes actual body read\n", SInfo, len(rawBody))
+}
+
+func renderIssuer(cert *x509.Certificate) string {
+	if cert.Issuer.String() == cert.Subject.String() {
+		return "<self-signed>"
+	}
+	return cert.Issuer.String()
+}
+
+func ips2str(ips []net.IP) []string {
+	ipStrs := []string{}
+	for _, ip := range ips {
+		ipStrs = append(ipStrs, ip.String())
+	}
+	return ipStrs
+}
+
+func nameInSans(name string, sans []string) bool {
+	for _, san := range sans {
+		if san == name {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO will be in stdlib anytime now... https://go-review.googlesource.com/c/go/+/321733/, https://github.com/golang/go/issues/46308
+func versionName(tlsVersion uint16) string {
+	switch tlsVersion {
+	case tls.VersionTLS10:
+		return "TLSv1.0"
+	case tls.VersionTLS11:
+		return "TLSv1.1"
+	case tls.VersionTLS12:
+		return "TLSv1.2"
+	case tls.VersionTLS13:
+		return "TLSv1.3"
+	default:
+		panic(errors.New("Unknown TLS version"))
+	}
+}
