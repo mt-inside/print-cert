1a1c76c05c7e4ca6da92b7d057f2c81d86eabe08
Matt Turner <user.personal@users.noreply.github.com>
2022-03-14 22:01:44 +0000

    Fix up colors

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/x509"
 	"fmt"
+	"net"
 	"os"
 	"strings"
 	"time"
@@ -13,43 +14,37 @@ import (
 const TimeFmt = "2006 Jan _2 15:04:05"
 
 var (
-	BrightStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("15")).Bold(true)
-	InfoStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Bold(true)
-	AddrStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Bold(true)
-	TimeStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("13")).Bold(true)
-	OkStyle     = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("10"))
-	WarnStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("11"))
-	FailStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("9"))
+	InfoStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("8"))
+	FailStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("1"))
+	OkStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("2"))
+	WarnStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("3"))
+	AddrStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("4"))
+	VerbStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("5"))
+	NounStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("6"))
+	BrightStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("7"))
 
-	SInfo    = InfoStyle.Render("Info:")
-	STrying  = BrightStyle.Render("Trying:")
-	SOk      = OkStyle.Render("Ok:")
-	SWarning = WarnStyle.Render("Warning:")
-	SError   = FailStyle.Render("Error:")
+	SInfo    = InfoStyle.Copy().Bold(true).Render("Info:")
+	STrying  = BrightStyle.Copy().Bold(true).Render("Trying:")
+	SOk      = OkStyle.Copy().Bold(true).Render("Ok:")
+	SWarning = WarnStyle.Copy().Bold(true).Render("Warning:")
+	SError   = FailStyle.Copy().Bold(true).Render("Error:")
 
-	SYes = OkStyle.Render("yes")
-	SNo  = FailStyle.Render("no")
+	SYes = OkStyle.Copy().Bold(true).Render("yes")
+	SNo  = FailStyle.Copy().Bold(true).Render("no")
 )
 
-func YesNo(test bool) string {
+func RenderYesNo(test bool) string {
 	if test {
 		return SYes
 	}
 	return SNo
 }
 
-func RenderList(ss []string) string {
-	if len(ss) == 0 {
-		return InfoStyle.Render("<none>")
-	}
-	return AddrStyle.Render(strings.Join(ss, ", "))
-}
-
-func RenderOptionalString(s string) string {
+func RenderOptionalString(s string, style lipgloss.Style) string {
 	if s == "" {
 		return InfoStyle.Render("<none>")
 	}
-	return BrightStyle.Render(s)
+	return style.Render(s)
 }
 
 func RenderTime(t time.Time, start bool) string {
@@ -70,12 +65,39 @@ func RenderTime(t time.Time, start bool) string {
 	}
 }
 
+func RenderList(ss []string) string {
+	if len(ss) == 0 {
+		return InfoStyle.Render("<none>")
+	}
+	return strings.Join(ss, ", ")
+}
+
+func RenderDNSList(names []string) string {
+	var ss []string
+	for _, name := range names {
+		ss = append(ss, AddrStyle.Render(name))
+	}
+	return RenderList(ss)
+}
+func RenderIPList(ips []net.IP) string {
+	var ss []string
+	for _, ip := range ips {
+		ss = append(ss, AddrStyle.Render(ip.String()))
+	}
+	return RenderList(ss)
+}
+
 func RenderCertBasics(cert *x509.Certificate) string {
-	return fmt.Sprintf("\t[%s -> %s] %s subj %s ca %t",
+	caFlag := InfoStyle.Render("non-ca")
+	if cert.IsCA {
+		caFlag = OkStyle.Render("ca")
+	}
+
+	return fmt.Sprintf("\t[%s -> %s] %s subj %s [%s]",
 		RenderTime(cert.NotBefore, true), RenderTime(cert.NotAfter, false),
-		BrightStyle.Render(cert.PublicKeyAlgorithm.String()), AddrStyle.Render(cert.Subject.String()),
+		NounStyle.Render(cert.PublicKeyAlgorithm.String()), AddrStyle.Render(cert.Subject.String()),
 		// No need to print Issuer, cause that's the Subject of the next cert in the chain
-		cert.IsCA,
+		caFlag,
 	)
 }
 
