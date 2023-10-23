0942cc0f5a9746be2e0141da986bf3f8eb9aedf4
Matt Turner <git@mt165.co.uk>
2023-10-23 23:10:06 +0100

    Clarify h/1.1 settings

--- pkg/probes/plaintext.go
+++ pkg/probes/plaintext.go
@@ -21,6 +21,8 @@ func buildPlaintextClient(
 			DialContext:           getDialContext(s, b, requestData, responseData),
 			ResponseHeaderTimeout: requestData.Timeout,
 			DisableCompression:    true,
+			// Note that this is pointless because Go's http2 won't work without TLS. This flag doesn't attempt anything. You can change this to an http2.Transport, and it'll tell you "http" is an invalid scheme.
+			ForceAttemptHTTP2: !requestData.HttpForce11, // Because there's no TLS, there's no ALPN, so we have to insist.
 		},
 	}
 
