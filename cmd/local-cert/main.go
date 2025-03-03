09b2ed6f963640419fb2ec9eb140402da9aa898b
Matt Turner <git@mt165.co.uk>
2025-03-03 16:10:31 +0000

    local-cert to read from stdin

--- cmd/local-cert/main.go
+++ cmd/local-cert/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/logrusorgru/aurora/v3"
@@ -15,10 +17,23 @@ func main() {
 	s := output.NewTtyStyler(aurora.NewAurora(true))
 	b := bios.NewTtyBios(s)
 
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: local-cert [file,-]")
+		os.Exit(2)
+	}
 	path := os.Args[1]
 
-	bytes, err := os.ReadFile(path)
+	var bytes []byte
+	var err error
+
+	if path == "-" {
+		si := bufio.NewReader(os.Stdin)
+		bytes, err = io.ReadAll(si)
+	} else {
+		bytes, err = os.ReadFile(path)
+	}
 	b.Unwrap(err)
+
 	certs, err := codec.ParseCertificates(bytes)
 	b.Unwrap(err)
 	fmt.Println(s.ServingCertChain(certs))
